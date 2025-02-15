b056942f7676304d7afbf88143dca4a57b8e0d66
Thomas Crowell <talexcrowell@gmail.com>
2025-02-15 11:56:15 -0700

    added basic equipment search for character sheet

--- src/components/CharacterSheet/pages/Equipment.tsx
+++ src/components/CharacterSheet/pages/Equipment.tsx
@@ -1,7 +1,11 @@
 import {
+  Accordion,
+  ActionIcon,
   Card,
+  Divider,
   Grid,
   Group,
+  HoverCard,
   Stack,
   Table,
   Text,
@@ -10,13 +14,38 @@ import {
   Title,
 } from "@mantine/core";
 import { useViewportContext } from "../../../contexts/ViewportContext";
-import { IconPlus, IconSearch } from "@tabler/icons-react";
+import {
+  IconDotsVertical,
+  IconInfoCircle,
+  IconPlus,
+  IconSearch,
+} from "@tabler/icons-react";
 import styles from "../../../Element.module.css";
 import { useState } from "react";
+import {
+  armorList,
+  otherGearAndServicesList,
+  vehicleList,
+  weaponsLists,
+} from "../../../data";
+import Fuse from "fuse.js";
 
 export const Equipment = () => {
   const [viewport] = useViewportContext();
   const [opened, setOpened] = useState(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const equipmentMasterList = [...weaponsLists, ...armorList, ...vehicleList];
+
+  const fuse = new Fuse(equipmentMasterList, {
+    keys: ["name"],
+    threshold: 0.45,
+  });
+
+  const filteredList = fuse.search(searchTerm).map((result) => {
+    return { ...result.item };
+  });
+
   return (
     <Grid py="md" px={viewport.width > 992 ? "md" : 0}>
       <Grid.Col span={12}>
@@ -24,26 +53,61 @@ export const Equipment = () => {
           <Title order={4} td="underline">
             Equipment
           </Title>
-          <Card
-            withBorder
-            styles={{ root: { borderStyle: "dotted", border: "1px" } }}
-            onClick={() => setOpened(!opened)}
+          
+          <Accordion
+            styles={{
+              root: { border: "1px solid #C9C9C9", borderRadius: "6px" },
+            }}
           >
-            <Card.Section className={styles.hoverElement}>
-              <Group c="dimmed" justify="center" py="md">
-                <IconPlus />
-                <Text>Add Equipment and Gear</Text>
-              </Group>
-            </Card.Section>
-            {opened && (
-              <Card.Section>
-                <TextInput leftSection={<IconSearch />} />
-                <Stack></Stack>
-              </Card.Section>
-            )}
-          </Card>
+            <Accordion.Item value={"add-equipment"}>
+              <Accordion.Control className={styles.hoverElement}>
+                <Group c="dimmed" justify="center" py="md">
+                  <IconPlus />
+                  <Text>Add Equipment and Gear</Text>
+                </Group>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Stack>
+                  <TextInput
+                    leftSection={<IconSearch />}
+                    placeholder="Search for name of equipment..."
+                    onChange={(e) => setSearchTerm(e.currentTarget.value)}
+                    label="Equipment Search"
+                  />
+                  <Divider />
+                  <Stack>
+                    {searchTerm ? (
+                      filteredList.length > 0 ? (
+                        filteredList.slice(0, 5).map((result) => {
+                          return (
+                            <Card>
+                              <Group justify="space-between">
+                                {result.name}{" "}
+                                <Group>
+                                  <ActionIcon>
+                                    <IconInfoCircle />
+                                  </ActionIcon>
+                                  <ActionIcon>
+                                    <IconPlus />
+                                  </ActionIcon>
+                                </Group>
+                              </Group>
+                            </Card>
+                          );
+                        })
+                      ) : (
+                        <Card c="dimmed">No results found</Card>
+                      )
+                    ) : (
+                      <Card c="dimmed">No results found</Card>
+                    )}
+                  </Stack>
+                </Stack>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
 
-          <Textarea label="Armor and Gear" rows={10} ta="start" />
+          <Textarea label="Other Gear" rows={10} ta="start" />
         </Stack>
       </Grid.Col>
       {/* <Grid.Col span={12}>
