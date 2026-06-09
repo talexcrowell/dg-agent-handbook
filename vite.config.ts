5aab1c736c0f9eb9ec11fb530e178d506fa2a35e
Thomas Crowell <talexcrowell@gmail.com>
2026-06-08 18:31:20 -0600

    reverted conffig

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  build: {
-    rollupOptions: {
-      treeshake: {
-        moduleSideEffects: false,
-        preset: "smallest",
-      },
-    },
-  },
   plugins: [react()],
 });
