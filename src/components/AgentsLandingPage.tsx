0394e0f0b8b9938d50938556b2328e2fd84e3add
Thomas Crowell <talexcrowell@gmail.com>
2026-07-06 15:28:37 -0600

    redesigned pages

--- src/components/AgentsLandingPage.tsx
+++ src/components/AgentsLandingPage.tsx
@@ -10,69 +10,81 @@ import {
 } from "@mantine/core";
 import { useCharacterContext } from "../contexts/CharacterContext";
 import { Link, NavLink } from "react-router-dom";
+import { useViewportContext } from "../contexts/ViewportContext";
 
 export const AgentsLandingPage = () => {
   const [{ currentCharacter }, actions] = useCharacterContext();
+  const [viewport] = useViewportContext();
+  const items = [
+    {
+      title: "Personnel File",
+      description:
+        "View your current Agent's file including their stats, skills, equipment, and their personal information. There are additional tools provided to help keep your Agent's profile stay up-to-date throughout operations.",
+      url: `/agents/sheet/${currentCharacter.codename}`,
+    },
+    {
+      title: "Agent Roster",
+      description:
+        "View the roster of Agents that you have recruited into the Program and other Agents on standby for last minute recruitment for an operation. Agent profiles stored on other devices can be imported here.",
+      url: "/agents/roster",
+    },
+    {
+      title: "Create an Agent",
+      description:
+        "Engage in a short guided process to create an Agent file for the Program's Agent Roster. The process will quantify the recruit's abilities and experience to create an fully interactable record of this Agent's tenure with the Program.",
+      url: "/agents/new",
+    },
+  ];
+
+  const DesktopCard = (item) => {
+    return (
+      <Card withBorder>
+        <Group justify="space-between">
+          <Stack gap="lg" w={"80%"}>
+            <Title ta="center" order={2} ta="start">
+              {item.title}
+            </Title>
+            <Text>{item.description}</Text>
+          </Stack>
+          <Button
+            variant="outline"
+            component={Link}
+            to={item.url}
+            size="xl"
+            w={"15%"}
+          >
+            View
+          </Button>
+        </Group>
+      </Card>
+    );
+  };
+
+  const MobileCard = (item) => {
+    return (
+      <Card withBorder>
+        <Stack gap="lg">
+          <Title ta="center" order={2}>
+            {item.title}
+          </Title>
+          <Text>{item.description}</Text>
+          <Button variant="outline" component={Link} to={item.url}>
+            View
+          </Button>
+        </Stack>
+      </Card>
+    );
+  };
 
   return (
     <Stack mt="sm">
       <Title>Agents Directory</Title>
       <Divider />
-      <Group justify="space-evenly">
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Personnel File
-            </Title>
-            <Text>
-              View your current Agent's file including their stats, skills,
-              equipment, and their personal information. There are additional
-              tools provided to help keep your Agent's profile stay up-to-date
-              throughout operations.
-            </Text>
-            <Button
-              variant="outline"
-              component={Link}
-              to={`/agents/sheet/${currentCharacter.codename}`}
-              disabled={!currentCharacter.codename}
-            >
-              View
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Agent Roster
-            </Title>{" "}
-            <Text>
-              View the roster of Agents that you have recruited into the Program
-              and other Agents on standby for last minute recruitment for an
-              operation. Agent profiles stored on other devices can be imported
-              here.
-            </Text>
-            <Button variant="outline" component={Link} to="/agents/roster">
-              View
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Create an Agent
-            </Title>
-            <Text>
-              Engage in a short guided process to create an Agent file for the
-              Program's Agent Roster. The process will quantify the recruit's
-              abilities and experience to create an fully interactable record of
-              this Agent's tenure with the Program.
-            </Text>
-            <Button variant="outline" component={Link} to="/agents/new">
-              Start
-            </Button>
-          </Stack>
-        </Card>
-      </Group>
+      <Stack>
+        {items.map((item) => {
+          return viewport.width > 600 ? DesktopCard(item) : MobileCard(item);
+        })}
+      </Stack>
     </Stack>
   );
 };
--- src/components/TrainingLandingPage.tsx
+++ src/components/TrainingLandingPage.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Card,
+  Center,
   Divider,
   Group,
   Stack,
@@ -8,101 +9,89 @@ import {
   Title,
 } from "@mantine/core";
 import { Link } from "react-router-dom";
+import { useViewportContext } from "../contexts/ViewportContext";
 
 export const TrainingLandingPage = () => {
+  const [viewport] = useViewportContext();
+  const items = [
+    {
+      title: "Training Guide",
+      description:
+        "Quick introduction into the game of Delta Green including the game system, the rules, and how to be an Agent for the Program.",
+      url: "/training/introduction",
+    },
+
+    {
+      title: "Stats and Skills",
+      description:
+        "Understand the deeper definitions and applications of an Agent's stats and skills. This can help shape an idea for a recruit for the Program.",
+      url: "/training/stats-and-skills",
+    },
+
+    {
+      title: "Professions",
+      description:
+        "Explore the variety of Agent professions and backgrounds that participate in the Program's operations.",
+      url: "/training/professions",
+    },
+
+    {
+      title: "Tradecraft",
+      description:
+        "Discover creative uses of your Agent's skills or services for all operations. It's always good to think outside the box.",
+      url: "/training/tradecraft",
+    },
+
+    {
+      title: "Glossary",
+      description:
+        "A comprehensive guide of terms for almost everything used by and associated with the Program.",
+      url: "/training/glossary",
+    },
+  ];
+
+  const DesktopCard = (item) => {
+    return (
+      <Card withBorder>
+        <Group justify="space-between">
+          <Stack gap="lg" w={'80%'}>
+            <Title ta="center" order={2} ta="start">
+              {item.title}
+            </Title>
+            <Text>{item.description}</Text>
+          </Stack>
+          <Button variant="outline" component={Link} to={item.url} size="xl" w={'15%'}>
+            View
+          </Button>
+        </Group>
+      </Card>
+    );
+  };
+
+  const MobileCard = (item) => {
+    return (
+      <Card withBorder>
+        <Stack gap="lg">
+          <Title ta="center" order={2}>
+            {item.title}
+          </Title>
+          <Text>{item.description}</Text>
+          <Button variant="outline" component={Link} to={item.url}>
+            View
+          </Button>
+        </Stack>
+      </Card>
+    );
+  };
   return (
     <Stack mt="sm">
       <Title>Training Directory</Title>
       <Divider />
-      <Group justify="space-evenly" align="top">
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Training Guide
-            </Title>
-            <Text>
-              Quick introduction into the game of Delta Green including the game
-              system, the rules, and how to be an Agent for the Program.
-            </Text>
-            <Button
-              variant="outline"
-              component={Link}
-              to="/training/introduction"
-            >
-              View Guide
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Stats and Skills
-            </Title>
-            <Text>
-              Understand the deeper definitions and applications of an Agent's
-              stats and skills. This can help shape an idea for a recruit for
-              the Program.
-            </Text>
-            <Button
-              variant="outline"
-              component={Link}
-              to="/training/stats-and-skills"
-            >
-              View Stats and Skills
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Professions
-            </Title>
-            <Text>
-              Explore the variety of Agent professions and backgrounds that
-              participate in the Program's operations.
-            </Text>
-            <Button
-              variant="outline"
-              component={Link}
-              to="/training/professions"
-            >
-              View Professions
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Tradecraft
-            </Title>
-            <Text>
-              Discover creative uses of your Agent's skills or services for all
-              operations. It's always good to think outside the box.
-            </Text>
-            <Button
-              variant="outline"
-              component={Link}
-              to="/training/tradecraft"
-            >
-              View Tradecraft
-            </Button>
-          </Stack>
-        </Card>
-        <Card withBorder w={325}>
-          <Stack gap="lg">
-            <Title ta="center" order={2}>
-              Glossary
-            </Title>
-            <Text>
-              A comprehensive guide of terms for almost everything used by and
-              associated with the Program.
-            </Text>
-            <Button variant="outline" component={Link} to="/training/glossary">
-              View Glossary
-            </Button>
-          </Stack>
-        </Card>
-      </Group>
+      <Stack>
+        {items.map((item) => {
+          return viewport.width > 600 ? DesktopCard(item) : MobileCard(item);
+        })}
+      </Stack>
     </Stack>
   );
 };
